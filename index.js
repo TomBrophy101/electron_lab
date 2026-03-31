ec6d64854c9cbfef62ea98548210f7d1f5308ed8
TomBrophy101 <tombrophy101@outlook.com>
2026-03-31 09:21:38 +0100

    updated Dev

--- index.js
+++ index.js
@@ -4,4 +4,12 @@
 //
 //  Created by Tom Brophy on 27/03/2026.
 //
-
+const { app, BrowserWindow } = require('electron');
+function createWindow() {
+    const win = new BrowserWindow({ width: 800, height: 600 });
+    win.loadFile('index.html');
+}
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
